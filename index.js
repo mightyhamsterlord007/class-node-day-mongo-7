a4f22fa1bdb9b91a4271c3cb3999bfc60bac0d2a
mightyhamsterlord007 <pak-code-teacher@jd-instructor.local>
2018-07-18 12:09:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/user', { useNewUrlParser: true }, function(err) {
+    if (err) {
+        console.log('Error: ', err);
+        return;
+    } 
+    console.log('MONGODB IS CONNECTED!');
+});
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, default: '' },
+    password: { type: String, default: '' }
+});
+
+const User = mongoose.model('UserSchema', UserSchema);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(morgan('dev'));
+
+app.get('/signup', function(req, res) {
+    res.send('This works');
+});
+
+app.post('/signup', function(req, res) {
+    User.create(req.body, function(err, result) {
+        if (err) {
+            res.json({
+                confirmation: 'Failure',
+                message: err
+            });
+        }
+        res.json({
+            confirmation: 'success',
+            data: result
+        });
+    });
+});
+
+app.post('/createanimal', function(req, res) {
+    
+})
+
+app.get('*', function(req, res) {
+    res.send('Page does not exist. Check your url');
+});
+
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(`Server is now running on PORT ${port}`);
+});
